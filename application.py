16705baa27e206fdaf7b2d2623eda807218ca54e
Bcuy <bcuy@usc.edu>
2020-05-31 19:29:19 -0700

    Added validation checks in form

--- application.py
+++ application.py
@@ -39,7 +39,13 @@ def register():
         return redirect(url_for('home'))
     return render_template('register.html', form=form)
 
-@app.route("/login")
+@app.route("/login", methods=['GET', 'POST'])
 def login():
     form = LoginForm()
+    if form.validate_on_submit():
+        if form.email.data == 'bcuy@usc.edu' and form.password.data == 'password':
+            flash('You have been logged in', 'success')
+            return redirect(url_for('home'))
+        else:
+            flash('Login unsuccessful. Please check email and password', 'danger')
     return render_template('login.html', form=form)
